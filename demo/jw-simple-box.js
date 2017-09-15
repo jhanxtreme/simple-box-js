c0b34b2ed6ed8e3fcee00cbebc71250dc8f9f113
Jhan Mateo <nino.jhan.n.mateo>
2017-09-15 21:37:41 +0800

    fixed issues - added feature to make the targeted element always at the center

--- demo/jw-simple-box.js
+++ demo/jw-simple-box.js
@@ -8,263 +8,248 @@
 $(document).ready(function(){
 	$.fn.simpleBox = function(config){
 
-		console.log('Simple Box Loaded.');
+		var targetElement = $(this);
 
-		var settings = {
-			config: {
-				animation: {
-					lib: 'default',
-					classes: 'animate'
-				},
-				targetType: null,
-			},
-			availableTargetTypes: ['images','iframes','forms','html'],
-			modal: {
-				ModalOuter: '.jw-simple-box',
-				ModalInner: '.jw-simple-box-inner',
-				ModalLoading: '.jw-simple-box .loading',
-				ModalTargetedInstance: null,
-				ModalTargetedInstanceName: '#targeted-element'
-			},
-			target: {
-				dimensions: {
-					current: {
-						width: 0,
-						height: 0
+		if(config.targetType == "images"){
+			images();
+		}else if(config.targetType == "modal"){
+			modal();
+		}else{
+			console.log("no target found");
+		}
+
+		function modal(){
+			console.log('the modal');
+			console.log(targetElement.html());
+		}
+
+		function images(){
+			console.log('Simple Box Loaded.', config);
+			var settings = {
+				config: {
+					animation: {
+						lib: 'default',
+						classes: 'animate'
 					},
-					original: {
+					targetType: null,
+				},
+				availableTargetTypes: ['images','iframes','forms','html'],
+				modal: {
+					ModalOuter: '.jw-simple-box',
+					ModalInner: '.jw-simple-box-inner',
+					ModalLoading: '.jw-simple-box .loading',
+					ModalTargetedInstance: null,
+					ModalTargetedInstanceName: '#targeted-element'
+				},
+				target: {
+					dimensions: {
+						current: {
+							width: 0,
+							height: 0
+						},
+						original: {
+							width: 0,
+							height: 0
+						}
+					},
+					window:{
 						width: 0,
 						height: 0
 					}
 				},
-				window:{
-					width: 0,
-					height: 0
-				}
-			},
-			autoWidths : ['100%', 'auto'],
-			css: {
-				inline: {
-					fullWidthPadded: { 'width': '80%', 'width' : 'calc(100% - 20%)', 'margin': '2em auto'},
-					fullWidth:  { 'width' : '100%'},
-					autoWidth: { 'width' : 'auto'},
-					fullHeight:  { 'height' : '100%'},
-					autoHeight: { 'height' : 'auto'},
-					vAlignCenter: {
-						'margin': 'auto',
-						'position': 'absolute',
-						'top': '0', 
-						'left': '0',
-						'bottom': '0',
-						'right': '0'
+				autoWidths : ['100%', 'auto'],
+				css: {
+					inline: {
+						fullWidthPadded90: { 'width': '90%', 'width' : 'calc(100% - 10%)', 'margin': '2em auto'},
+						marginCenter2emAuto: {'margin': '2em auto'},
+						autoWidth: { 'width' : 'auto'},
+						fullHeight:  { 'height' : '100%'},
+						autoHeight: { 'height' : 'auto'},
+						vAlignCenter: { 'margin': 'auto', 'position': 'absolute', 'top': '0', 'left': '0','bottom': '0', 'right': '0' },
+						modalOuterOverflow: { 'overflow-x': 'hidden', 'overflow-y': 'auto' },
+						overflowHidden: {'overflow': 'hidden'}
 					},
-					modalOuterOverflow: {
-						'overflow-x': 'hidden',
-						'overflow-y': 'auto',
-					},
-					overflowHidden: {'overflow': 'hidden'}
+					displayClassName: "show",
 				},
-				displayClassName: "show",
-			},
-			attributes:{
-				dataSource: 'data-source'
-			},
-			setVerticalAlignCenter: false,
-			messages:{
-				typeNotFound: "Target type not found"
-			},
-			timeout: {
-				instance: null,
-				ms: 100
-			}
-		};
+				attributes:{
+					dataSource: 'data-source'
+				},
+				setVerticalAlignCenter: false,
+				messages:{
+					typeNotFound: "Target type not found"
+				},
+				timeout: {
+					instance: null,
+					ms: 100
+				}
+			};
 
-		var overRideConfigurations = function(paramsObj){
-			for(var name in paramsObj){
-				settings.config[name] = paramsObj[name];
-			}
-		};
+			var overRideConfigurations = function(paramsObj){
+				for(var name in paramsObj){
+					settings.config[name] = paramsObj[name];
+				}
+			};
 
-		var closeModal = function(){
-			console.log('modal closed.');
-			settings.setVerticalAlignCenter = false;
-			settings.modal.ModalTargetedInstance = 0;
-			if($(settings.modal.ModalOuter + ' ' + settings.modal.ModalTargetedInstanceName).length !== 0){
-				$(settings.modal.ModalOuter + ' ' + settings.modal.ModalTargetedInstanceName).remove();
-				settings.modal.ModalTargetedInstance = null;
-			}
-			$(settings.modal.ModalOuter).removeClass(settings.css.displayClassName);
-			$(settings.modal.ModalInner).removeClass(settings.css.displayClassName).removeAttr('style');
-			$('body').removeAttr('style');
-		};
+			var closeModal = function(){
+				console.log('modal closed.');
+				settings.setVerticalAlignCenter = false;
+				settings.modal.ModalTargetedInstance = 0;
+				if($(settings.modal.ModalOuter + ' ' + settings.modal.ModalTargetedInstanceName).length !== 0){
+					$(settings.modal.ModalOuter + ' ' + settings.modal.ModalTargetedInstanceName).remove();
+					settings.modal.ModalTargetedInstance = null;
+				}
+				$(settings.modal.ModalOuter).removeClass(settings.css.displayClassName).removeAttr('style');
+				$(settings.modal.ModalInner).removeClass(settings.css.displayClassName).removeAttr('style');
+				$('body').removeAttr('style');
+			};
 
-		var removeInlineStyles = function(){
-			//reset inlinestyles
-			$(settings.modal.ModalOuter).removeAttr('style');
-			$(settings.modal.ModalInner).removeAttr('style');
-			$(settings.modal.ModalTargetedInstance).removeAttr('style');
-		};
+			var removeInlineStyles = function(){
+				//reset inlinestyles
+				$(settings.modal.ModalOuter).removeAttr('style');
+				$(settings.modal.ModalInner).removeAttr('style');
+				$(settings.modal.ModalTargetedInstance).removeAttr('style');
+			};
 
-		var renderModal = function(){
-			//disable the body scrolling feature
-			$('body').css(settings.css.inline.overflowHidden);
+			var showModal = function(){
+				//remove loading from the screen
+				$(settings.modal.ModalLoading).removeClass(settings.css.displayClassName);
+				//show the inner content
+				$(settings.modal.ModalInner).addClass(settings.css.displayClassName);
+				//disable the body scrolling feature
+				$('body').css(settings.css.inline.overflowHidden);
+				//add css scroll 
+				if(settings.timeout.instance){
+					clearTimeout(settings.timeout);
+				}
+				//delay the display after the animation
+				settings.timeout.instance = setTimeout(function(){
+					$(settings.modal.ModalOuter).css(settings.css.inline.modalOuterOverflow);
+				}, settings.timeout.ms);
+			};
 
-			//add css scroll 
-			if(settings.timeout.instance){
-				clearTimeout(settings.timeout);
-			}
+			var calcContentPosition = function(){
 
-			//delay the display after the animation
-			settings.timeout.instance = setTimeout(function(){
-				$(settings.modal.ModalOuter).css(settings.css.inline.modalOuterOverflow);
-			}, settings.timeout.ms);
-		};
+				var calcHorizontalCenter = function(){
+					$(settings.modal.ModalInner).append(settings.modal.ModalTargetedInstance);
+					var ModalInnerHeight = $(settings.modal.ModalInner).height();
 
-		var showBox = function(){
+					if(ModalInnerHeight <= window.innerHeight){
+						$(settings.modal.ModalInner).css(settings.css.inline.vAlignCenter);
+						$(settings.modal.ModalTargetedInstance).css(settings.css.inline.vAlignCenter);
+					}
+				};
 
-			//reset inlinestyles
-			removeInlineStyles();
+				//window.width > target.width && window.height > target.height
+				if(window.innerWidth > settings.target.dimensions.original.width &&
+					window.innerHeight > settings.target.dimensions.original.height ){
+					$(settings.modal.ModalTargetedInstance).css(settings.css.inline.vAlignCenter);
+					$(settings.modal.ModalInner).css(settings.css.inline.vAlignCenter);
+				}
 
-			//if window width is lesser than source width
-			if(window.innerWidth < settings.target.dimensions.original.width){
-				console.log('responsive activate width!');
-				settings.target.dimensions.current.width = 'auto';
-				$(settings.modal.ModalTargetedInstance).css(settings.css.inline.fullWidthPadded);
-			}else{
-				settings.target.dimensions.current.width = settings.target.dimensions.original.width + 'px';
-				settings.target.dimensions.current.height = settings.target.dimensions.original.height + 'px';
-			}
+				//window.width > target.width && window.height < target.height
+				if(window.innerWidth > settings.target.dimensions.original.width &&
+					window.innerHeight < settings.target.dimensions.original.height){
+					$(settings.modal.ModalTargetedInstance).css(settings.css.inline.marginCenter2emAuto);
+				}
 
-			//if window height is lesser than source height
-			if(window.innerHeight < settings.target.dimensions.original.height){
-				settings.target.dimensions.current.height = 'auto';
-			}else{
-				settings.target.dimensions.current.height = settings.target.dimensions.original.height + 'px';
-				settings.setVerticalAlignCenter = true;
-			}
+				//window.width < target.width (for large sizes)
+				if(window.innerWidth <= settings.target.dimensions.original.width){
+					$(settings.modal.ModalTargetedInstance).css(settings.css.inline.fullWidthPadded90);
+				}
 
-			//if targeted elements height is lesser than the window height, then make it center
-			if(settings.setVerticalAlignCenter){
-				$(settings.modal.ModalTargetedInstance).css(settings.css.inline.vAlignCenter);
-				$(settings.modal.ModalInner).css(settings.css.inline.vAlignCenter);
-			}
-
-			//apply the css styles 
-			$(settings.modal.ModalInner)
-				.append(settings.modal.ModalTargetedInstance)
-				.css({ 'width' : settings.target.dimensions.current.width, 'height' : settings.target.dimensions.current.height})
-				.addClass(settings.css.displayClassName);
-
-			//remove loading from the screen
-			$(settings.modal.ModalLoading).removeClass(settings.css.displayClassName);
-
-			renderModal();
+				//calculate center
+				calcHorizontalCenter();
 		
-		};
+			};
 
-		var loadModalElements = function(){
-			//check if parent modal has exists in the body
-			if($(settings.modal.ModalOuter).length == 0){
-				$('<div class="jw-simple-box transparent" id="jw-simple-box"><div class="loading spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div><div class="jw-simple-box-inner"></div></div>').prependTo('body');
-
-				//apply css animations
-				$(settings.modal.ModalInner).addClass(settings.config.animation.classes);
-
-			}else{
-				console.log('PopBox already loaded.');
-			}
-		};
-
-		var preLoadModal = function(){
-			//show the modal
-			if(settings.availableTargetTypes.indexOf(settings.config.targetType) > -1){
-				$(settings.modal.ModalOuter).addClass(settings.css.displayClassName);
-				$(settings.modal.ModalLoading).addClass(settings.css.displayClassName);
-			}else{
-				console.log(settings.messages.typeNotFound);
-			}
-		};
 			
-		var listenClick = function(elem){
-			elem.click(function(e){
-				e.preventDefault();
+			var calibrateContent = function(){
+				//reset inlinestyles
+				removeInlineStyles();
+				//calculate contenet position
+				calcContentPosition();			
+				//show the modal
+				showModal();
+			};
 
-				preLoadModal();
+			var loadModalElements = function(){
+				//check if parent modal has exists in the body
+				if($(settings.modal.ModalOuter).length == 0){
+					$('<div class="jw-simple-box transparent" id="jw-simple-box"><div class="loading spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div><div class="jw-simple-box-inner" id="jw-simple-box-inner"></div></div>').prependTo('body');
 
+					//apply css animations
+					$(settings.modal.ModalInner).addClass(settings.config.animation.classes);
+
+				}else{
+					console.log('PopBox already loaded.');
+				}
+			};
+
+			var preLoadModal = function(){
+				//show the modal
 				if(settings.availableTargetTypes.indexOf(settings.config.targetType) > -1){
-					var imageInstance = $(this).attr(settings.attributes.dataSource);
-					settings.modal.ModalTargetedInstance = new Image();
-					settings.modal.ModalTargetedInstance.src = imageInstance;
-					settings.modal.ModalTargetedInstance.setAttribute('id', 'targeted-element');
-					settings.modal.ModalTargetedInstance.onload = function(){
-						//set current dimensions
-						settings.target.dimensions.current.width = this.naturalWidth;
-						settings.target.dimensions.current.height = this.naturalHeight;
-						//store original dimensions
-						settings.target.dimensions.original.width = settings.target.dimensions.current.width;
-						settings.target.dimensions.original.height = settings.target.dimensions.current.height;
-						//trigger the box
-						showBox();
-					};
+					$(settings.modal.ModalOuter).addClass(settings.css.displayClassName);
+					$(settings.modal.ModalLoading).addClass(settings.css.displayClassName);
 				}else{
 					console.log(settings.messages.typeNotFound);
 				}
+			};
+				
+			var listenClick = function(elem){
+				elem.click(function(e){
+					e.preventDefault();
 
-			});
+					preLoadModal();
 
-			$(settings.modal.ModalOuter).on('click', function(e){
-				console.log(e.target.id);
-				if(e.target.id != "targeted-element"){
-					closeModal();
-				}
-			});
-		
-		};
-
-		var responseWhenResize = function(){
-			$(window).on('resize', function(){
-
-				//get window width
-				settings.target.window.width = $(this).width();
-
-				//get window height
-				settings.target.window.height = $(this).height();
-
-				//get current width of the targeted element				
-				var elementWidth = (settings.autoWidths.indexOf(settings.target.dimensions.current.width) > -1 ) ? settings.target.dimensions.current.width : parseInt((settings.target.dimensions.current.width + "").replace(/px/,''));
-
-				//check element is available
-				if(elementWidth !== 0){
-
-					//reset inline css styles
-					removeInlineStyles();
-
-					//if the targeted element's width is greater than the window, make the width 100%
-					if(settings.target.dimensions.original.width > settings.target.window.width){
-						$(settings.modal.ModalInner).css(settings.css.inline.fullWidthPadded);
-						$(settings.modal.ModalTargetedInstance).css(settings.css.inline.fullWidth);
+					if(settings.availableTargetTypes.indexOf(settings.config.targetType) > -1){
+						var imageInstance = $(this).attr(settings.attributes.dataSource);
+						settings.modal.ModalTargetedInstance = new Image();
+						settings.modal.ModalTargetedInstance.src = imageInstance;
+						settings.modal.ModalTargetedInstance.setAttribute('id', 'targeted-element');
+						settings.modal.ModalTargetedInstance.onload = function(){
+							//set current dimensions
+							settings.target.dimensions.current.width = this.naturalWidth;
+							settings.target.dimensions.current.height = this.naturalHeight;
+							//store original dimensions
+							settings.target.dimensions.original.width = settings.target.dimensions.current.width;
+							settings.target.dimensions.original.height = settings.target.dimensions.current.height;
+							//trigger the box
+							calibrateContent();
+						};
 					}else{
-						//reset all css styles to default
-						$(settings.modal.ModalInner).css({ 'width' : settings.target.dimensions.original.width + 'px', 'margin': '0 auto !important'});
-						$(settings.modal.ModalTargetedInstance).css(settings.css.inline.autoWidth);
+						console.log(settings.messages.typeNotFound);
 					}
 
-					//targeted element align to center of the screen
-					if(settings.setVerticalAlignCenter){
-						$(settings.modal.ModalTargetedInstance).css(settings.css.inline.vAlignCenter);
-						$(settings.modal.ModalInner).css(settings.css.inline.vAlignCenter);
+				});
+
+				$(settings.modal.ModalOuter).on('click', function(e){
+					console.log(e.target.id);
+					if(e.target.id != "targeted-element"){
+						closeModal();
 					}
+				});
+			
+			};
 
-					renderModal();
+			var responseWhenResize = function(){
+				$(window).on('resize', function(){
+					//get window width
+					settings.target.window.width = $(this).width();
+					//get window height
+					settings.target.window.height = $(this).height();
+					//get current width of the targeted element				
+					var elementWidth = (settings.autoWidths.indexOf(settings.target.dimensions.current.width) > -1 ) ? settings.target.dimensions.current.width : parseInt((settings.target.dimensions.current.width + "").replace(/px/,''));
+					//check element is available
+					if(elementWidth !== 0){
+						calibrateContent();
+					}
+				});
+			};
 
-				}
-
-			});
-		};
-
-		overRideConfigurations(config);
-		loadModalElements();
-		responseWhenResize();
-		listenClick($(this));
+			overRideConfigurations(config);
+			loadModalElements();
+			responseWhenResize();
+			listenClick(targetElement);
+		}
 	};
 });
